cb123bda4c7af877fbcfc9f8a1855c0991aaeda6
svorcmar <svorcmar@gmail.com>
2023-11-19 11:23:30 +0100

    Create d4/d4_1.kt

---
+++ src/d4/d4_1.kt
@@ -0,0 +1,12 @@
+fun main() {
+  val input = ""
+  var solution = -1
+  generateSequence(0) { it + 1 }.takeWhile { solution == -1 }.map { 
+    val md5 = java.security.MessageDigest.getInstance("md5")
+      .digest((input + it).toByteArray())
+      .joinToString("") { "%02x".format(it) }
+    if (md5.startsWith("00000"))
+      solution = it
+  }.last()
+  println(solution)
+}
