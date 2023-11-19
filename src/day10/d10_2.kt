f02156881ccd78f68e152be51e118853c3cc2a25
svorcmar <svorcmar@gmail.com>
2023-11-19 17:34:30 +0100

    Create d10_2.kt

---
+++ src/day10/d10_2.kt
@@ -0,0 +1,20 @@
+fun main() {
+  val input = ""
+  println(generateSequence(input) { lookAndSay(it) }.take(51).last().length)
+}
+
+fun lookAndSay(s: String): String {
+  var prev = 'x'
+  var cnt = 1
+  val buffer = StringBuilder()
+  for (ch in s) {
+    if (ch == prev) {
+      cnt++
+    } else {
+      buffer.append(cnt).append(prev)
+      cnt = 1
+      prev = ch
+    }
+  }
+  return buffer.append(cnt).append(prev).toString().substring(2)
+}
