a26741bb75656abe23acaf4d7d81943dbb344484
svorcmar <svorcmar@gmail.com>
2023-11-20 10:28:11 +0100

    Complete d12_2.kt

--- src/day12/d12_2.kt
+++ src/day12/d12_2.kt
@@ -1,23 +1,40 @@
 fun main() {
   val input = ""
-  val obj = parseObj(input, 1)
+  val obj = parseObj(input, 1, false)
   println(obj.sum())
 }
 
-fun parseObj(input: String, from: Int): ObjectFragment {
+fun parseObj(input: String, from: Int, isArray: Boolean): ObjectFragment {
   val sb = StringBuilder()
   val arrSb = StringBuilder()
   val subObjects = mutableListOf<ObjectFragment>()
   var i = from
   while(true) {
     i += when(input[i]) {
-      '['  -> TODO()
-      ']'  -> TODO()
-      '{'  -> {
-        subObjects.add(parseObj(input, i + 1))
+      '['  -> {
+        subObjects.add(parseObj(input, i + 1, true))
         subObjects.last().length
       }
-      '}'  -> return ObjectFragment(subObjects, sb.insert(0, '{').append('}').toString()).also { println(it.remainder) }
+      ']'  -> {
+        if (isArray)
+          return ObjectFragment(subObjects, sb.insert(0, '[').append(']').toString(), true)
+        else {
+          sb.append(input[i])
+          1
+        }
+      }
+      '{'  -> {
+        subObjects.add(parseObj(input, i + 1, false))
+        subObjects.last().length
+      }
+      '}'  -> {
+        if (!isArray)
+          return ObjectFragment(subObjects, sb.insert(0, '{').append('}').toString(), false)
+        else {
+          sb.append(input[i])
+          1
+        }
+      }
       else -> {
         sb.append(input[i])
         1
@@ -26,17 +43,17 @@ fun parseObj(input: String, from: Int): ObjectFragment {
   }
 }
 
-data class ObjectFragment(val subObjects: List<ObjectFragment>, val remainder: String, val ignoredRemainder: String) {
+data class ObjectFragment(val subObjects: List<ObjectFragment>, val remainder: String, val isArray: Boolean) {
     
   val length: Int by lazy { subObjects.map { it.length }.sum() + remainder.length }
   
   fun sum(): Int {
-    return if (remainder.contains("\"red\""))
+    return if (!isArray && remainder.contains("\"red\""))
       0
     else
-      sumString(remainder) + sumString(ignoredRemainder) + subObjects.map { it.sum() }.sum()
+      sumRemainder() + subObjects.map { it.sum() }.sum()
   }
   
-  private fun sumString(s: String): Int =
-    "(-?[0-9]+)".toRegex().findAll(s).map { it.groupValues[1].toInt() }.sum()
+  private fun sumRemainder(): Int =
+    "(-?[0-9]+)".toRegex().findAll(remainder).map { it.groupValues[1].toInt() }.sum()
 }
