2cb2a1031cb20183ff594e888646ce0a28212443
svorcmar <svorcmar@gmail.com>
2023-11-21 07:58:58 +0100

    Update d15_1.kt

--- src/day15/d15_1.kt
+++ src/day15/d15_1.kt
@@ -1,16 +1,16 @@
 fun main() {
-  val input = """Sugar: capacity 3, durability 0, flavor 0, texture -3, calories 2
-Sprinkles: capacity -3, durability 3, flavor 0, texture 0, calories 9
-Candy: capacity -1, durability 0, flavor 4, texture 0, calories 1
-Chocolate: capacity 0, durability 0, flavor -2, texture 2, calories 8"""
+  val input = ""
   val ingredients = input.lines().map { parseIngredient(it) }.associateBy { it.name }
-  /*forAllCombinations(ingredients, 100) { it.map { (k, v) -> listOf(
-    ingredients[k]!!.capacity * v,
-    ingredients[k]!!.durability * v,
-    ingredients[k]!!.flavor * v,
-    ingredients[k]!!.texture * v) }.fold(listOf(0, 0, 0, 0) { acc, l -> acc.zip(l) { a, b -> a + b } } ) } */
-  println(ingredients)
-  forAllCombinations(ingredients, 100) { }.forEach{ println }
+  println(forAllCombinations(ingredients, 100) { 
+    it.map { (k, v) -> listOf(
+      ingredients[k]!!.capacity * v,
+      ingredients[k]!!.durability * v,
+      ingredients[k]!!.flavor * v,
+      ingredients[k]!!.texture * v)
+    }.fold(listOf(0, 0, 0, 0)) { acc, list -> acc.zip(list) { a, b -> a + b } }
+     .map { if (it < 0) 0 else it }
+     .reduce(Int::times) 
+  }.max())
 }
 
 data class Ingredient(val name: String, val capacity: Int, val durability: Int, val flavor: Int, val texture: Int)
@@ -30,15 +30,17 @@ fun <T> forAllCombinations(ingredients: Map<String, Ingredient>, n: Int, f: (Map
 fun <T> forAllCombinationsRec(step: Int, stepCount: Int, currIdx: Int,
                               arr: Array<Int>, names: List<String>,
                               f: (Map<String, Int>) -> T): Sequence<T> = sequence {
-  if (step == stepCount) 
+  if (step == stepCount) {
     yield(f(arr.mapIndexed {i, cnt -> names[i] to cnt }.toMap()))
-  if (currIdx < arr.size - 1) {
-    arr[currIdx + 1]++
-    yieldAll(forAllCombinationsRec(step + 1, stepCount, currIdx + 1, arr, names, f))
-    arr[currIdx + 1]--
+  } else {
+    if (currIdx < arr.size - 1) {
+      arr[currIdx + 1]++
+      yieldAll(forAllCombinationsRec(step + 1, stepCount, currIdx + 1, arr, names, f))
+      arr[currIdx + 1]--
+    }
+    arr[currIdx]++
+    yieldAll(forAllCombinationsRec(step + 1, stepCount, currIdx, arr, names, f))
+    arr[currIdx]--
   }
-  arr[currIdx]++
-  yieldAll(forAllCombinationsRec(step + 1, stepCount, currIdx, arr, names, f))
-  arr[currIdx]--
 }
   
