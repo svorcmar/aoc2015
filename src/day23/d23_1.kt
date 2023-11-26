a55cb20eadc7f27961a0872d9c430e04b0370b89
svorcmar <svorcmar@gmail.com>
2023-11-26 12:12:00 +0100

    Create d23_1.kt

---
+++ src/day23/d23_1.kt
@@ -0,0 +1,50 @@
+fun main() {
+  val input = ""
+  val instrs = input.lines().map { it.split(" ").let { Triple(it[0], 
+                                                              if (it.size == 2) it[1] else it[1].dropLast(1),
+                                                              if (it.size == 3) it[2] else "") } }
+  var pc = 0
+  var a = 0L
+  var b = 0L
+  while (pc < instrs.size) {
+    var pcD = 1
+    when(instrs[pc].first) {
+      "hlf" -> {
+        if (instrs[pc].second == "a")
+          a /= 2
+        else
+          b /= 2
+      }
+      "tpl" -> {
+        if (instrs[pc].second == "a")
+          a *= 3
+        else
+          b *= 3
+      }
+      "inc" -> {
+        if (instrs[pc].second == "a")
+          a++
+        else
+          b++
+      }
+      "jmp" -> {
+        pcD = instrs[pc].second.toInt()
+      }
+      "jie" -> {
+        if (instrs[pc].second == "a" && a.mod(2) == 0)
+          pcD = instrs[pc].third.toInt()
+        if (instrs[pc].second == "b" && b.mod(2) == 0)
+          pcD = instrs[pc].third.toInt()
+      }
+      "jio" -> {
+        if (instrs[pc].second == "a" && a == 1L)
+          pcD = instrs[pc].third.toInt()
+        if (instrs[pc].second == "b" && b == 1L)
+          pcD = instrs[pc].third.toInt()
+      }
+      else -> throw IllegalArgumentException(instrs[pc].first)
+    }
+    pc += pcD
+  }
+  println(b)
+}
